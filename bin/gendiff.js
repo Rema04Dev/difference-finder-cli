390e9e5cd7c9be9b963f993fd37f0374f2d5d324
Rema04Dev <romansmerdyakov@gmail.com>
2022-04-09 22:37:06 +0300

    add new option to CLI

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,13 +3,9 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
-  .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<type>')
+  .option('-f --format <type>', 'output format')
 
-  program.command('s')
-  .action((str, options) => {
-    const limit = options.first ? 1 : undefined;
-    console.log(str.split(options.separator, limit));
-  });
-  
-  program.parse();
+  program.parse(process.argv);
