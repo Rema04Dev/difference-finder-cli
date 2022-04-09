902f6c832c80e3e66c88174b339ef40b446b68c1
Rema04Dev <romansmerdyakov@gmail.com>
2022-04-10 01:06:14 +0300

    add JSON files

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import _ from 'lodash';
+import fs from 'fs';
 const program = new Command();
 
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .arguments('<type>')
+  // .arguments('<type>')
   .option('-f --format <type>', 'output format')
 
   program.parse(process.argv);
