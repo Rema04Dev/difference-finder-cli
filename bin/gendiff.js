039b4672a5df0cc2da552fbd01e6218a210d9479
Rema04Dev <romansmerdyakov@gmail.com>
2022-04-10 12:16:03 +0300

    draft commit

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,7 @@ const program = new Command();
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  // .arguments('<type>')
+  .arguments('<type>')
   .option('-f --format <type>', 'output format')
 
   program.parse(process.argv);
