c1f3afa70b11b872dff7d329f2de19d1bc87e042
Rema04Dev <romansmerdyakov@gmail.com>
2022-04-17 10:58:42 +0300

    refactor

--- __tests__/compare.test.js
+++ __tests__/compare.test.js
@@ -1,20 +1,16 @@
-import compareFiles from '../src/compare.js'
 import path from 'path';
-import sum from '../src/draft.js';
+import compareFiles from '../src/compare.js';
+
 const createPath = (filename) => path.join('./', '__fixtures__', filename);
 test('Compare', () => {
-    expect(compareFiles(createPath('file1.json'), createPath('file2.json'))).toEqual({
-        "+age": 32,
-        "+children": "children",
-        "+married": true,
-        "+name": "Ivan",
-        "-age": 30,
-        "-married": false,
-        "-name": "ALexandr",
-        "city": "Moscow"
-    })
-})
-
-test('Sum', () => {
-    expect(sum(4, 5)).toBe(9)
-})
+  expect(compareFiles(createPath('file5.json'), createPath('file6.json'))).toEqual({
+    '+age': 32,
+    '-children': 'children',
+    '+married': true,
+    '+name': 'Ivan',
+    '-age': 30,
+    '-married': false,
+    '-name': 'ALexandr',
+    city: 'Moscow',
+  });
+});
--- src/compare.js
+++ src/compare.js
@@ -8,15 +8,16 @@ const getObjectWithMatualKeys = (object1, object2) => {
   const keys1 = getKeys(object1);
   const keys2 = getKeys(object2);
   const commonKeys = _.intersection(keys1, keys2);
-  const result = {};
-  for (const key of commonKeys) {
+  console.log(commonKeys);
+  const result = commonKeys.reduce((acc, key) => {
     if (object1[key] !== object2[key]) {
-      result[`-${key}`] = object1[key];
-      result[`+${key}`] = object2[key];
+      acc[`-${key}`] = object1[key];
+      acc[`+${key}`] = object2[key];
     } else {
-      result[key] = object1[key];
+      acc[key] = object1[key];
     }
-  }
+    return acc;
+  }, {});
   return result;
 };
 
@@ -25,19 +26,20 @@ const getDifferentKeys = (object1, object2) => {
   const keys2 = getKeys(object2);
   const uniqueKeysFromObject1 = _.difference(keys1, keys2);
   const uniqueKeysFromObject2 = _.difference(keys2, keys1);
-  const result = {};
-  for (const key of uniqueKeysFromObject1) {
-    result[`-${key}`] = key;
-  }
-  for (const key of uniqueKeysFromObject2) {
-    result[`+${key}`] = key;
-  }
-  return result;
+  const keysOfObject1 = uniqueKeysFromObject1.reduce((acc, key) => {
+    acc[`-${key}`] = key;
+    return acc;
+  }, {});
+  const keysOfObject2 = uniqueKeysFromObject2.reduce((acc, key) => {
+    acc[`-${key}`] = key;
+    return acc;
+  }, {});
+  return Object.assign(keysOfObject1, keysOfObject2);
 };
 
 const compareFiles = (filepath1, filepath2) => {
   const data1 = fs.readFileSync(filepath1, 'utf-8');
-  const data2 = fs.readFileSync(filepath2, 'utf-8');  
+  const data2 = fs.readFileSync(filepath2, 'utf-8');
   const object1 = parseData(data1);
   const object2 = parseData(data2);
 
@@ -45,7 +47,8 @@ const compareFiles = (filepath1, filepath2) => {
     getObjectWithMatualKeys(object1, object2),
     getDifferentKeys(object1, object2),
   );
+  console.log(result);
   return result;
 };
-
+compareFiles('__fixtures__/file1.json', '__fixtures__/file2.json');
 export default compareFiles;
