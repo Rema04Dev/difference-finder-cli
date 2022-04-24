1e0348895eb225caa880a3699da920f7542a48ab
Rema04Dev <romansmerdyakov@gmail.com>
2022-04-24 12:51:33 +0300

    fix options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,7 @@ const program = new Command();
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', '(default: "stylish")')
   .arguments('<filepath>', 'filepath to first file')
   .arguments('<file2>', 'filepath to second file')
   .action((filepath1, filepath2) => {
