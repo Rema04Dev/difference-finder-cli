1d7d925b9fe4de01709c4b53f76d27def3ae661d
Rema04Dev <romansmerdyakov@gmail.com>
2022-06-29 23:37:24 +0300

    fix json output

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import parse from './parsers.js';
 import compare from './compare.js';
 import getRenderFormat from './formatters/index.js';
 
+const getFullPath = (filepath) => path.resolve(process.cwd(), filepath)
 const parseData = (filepath) => {
   const ext = path.extname(filepath);
-  const data = fs.readFileSync(path.resolve(filepath), 'utf-8');
+  const data = fs.readFileSync(getFullPath(filepath), 'utf-8');
   return parse(data, ext);
 };
 const gendiff = (filepath1, filepath2, formatName = 'stylish') => {
